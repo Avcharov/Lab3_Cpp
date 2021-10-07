1cab129080993dd77a9953e1cb223fbd9f7176fd
Admin <Admin@XIAOMI>
2021-10-07 17:56:14 +0300

    Добавьте файлы проекта.

---
+++ Lab3_Cpp/Lab3_Cpp.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+int main()
+{
+	double x;
+	cin >> x;
+		double sum = 0;
+	double k = 0.0001;
+	int n = 0;
+	double step = x;
+	double fact=1;
+	int sign = 1;
+		while (abs(sign*step/fact)>k) {
+			sum += sign * step / fact;
+           n++;
+		   sign = -sign;
+		   step *= x * x;
+		   fact *= 2 * n;
+		   
+		}
+		n--;
+		cout << sum <<" "<< n;
+	
+    
+	
+}
+
